a72f44b8ae1a04de3ae9347b3f637aee3c2f94a9
Elías Rodríguez Martín <alu0100456608@ull.edu.es>
2013-10-18 17:15:49 +0100

    Implementada clase Fraccion y conjunto de pruebas unitarias

--- racional.rb
+++ racional.rb
@@ -3,4 +3,92 @@
 require "./gcd.rb"
 
 class Fraccion
+   #Declaración de los accesors para el numerador y el denominador
+   attr_reader :num, :denom
+   #Constructor que permite inicializar la fraccion con los valores indicados.
+   #La fracción almacenada será el resultado de reducirla a su mínima expreción
+   def initialize (num,denom)
+      @num = num/gcd(num,denom)
+      @denom = denom/gcd(num,denom)
+   end
+  
+   #Devuelve esta fracción reducida a su mínima expreción
+   def reducir
+      num = @num/gcd(@num,@denom)
+      denom = @denom/gcd(@num,@denom) 
+      
+      Fraccion.new(num,denom)
+   end
+  
+   #Devuelve una cadena con la representación del racional
+   def to_s
+      "#{@num}/#{@denom}"
+   end
+  
+   #Devuelve un nuevo racional que suma al objeto que invoca el que le pasan como parámetro
+   def suma(fraccion)
+      #Se calculan los nuevos valores
+      num = @num*fraccion.denom + @denom*fraccion.num
+      denom = @denom*fraccion.denom
+    
+      res = Fraccion.new(num,denom)
+      
+      #Se reduce la fraccion
+      res.reducir
+   end
+  
+   #Devuelve un nuevo racional que resta al objeto que invoca el que le pasan como parámetro
+   def resta(fraccion)
+      num = @num*fraccion.denom - @denom*fraccion.num
+      denom = @denom*fraccion.denom
+    
+      res = Fraccion.new(num,denom)
+      
+      #Se reduce la fraccion
+      res.reducir
+   end
+  
+   #Devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+   def producto(fraccion)
+      num = @num*fraccion.num
+      denom = @denom*fraccion.denom
+    
+      res = Fraccion.new(num,denom)
+      
+      #Se reduce la fraccion
+      res.reducir
+   end
+  
+   #Devuelve un nuevo racional que multiplica al objeto que invoca el que le pasan como parámetro
+   def division(fraccion) 
+      num = @num*fraccion.denom
+      denom = @denom*fraccion.num
+      
+      res = Fraccion.new(num,denom)
+      
+      #Se reduce la fraccion
+      res.reducir
+   end
+  
+   #Sobrecarga del operador == para comprobar igualdad entre fracciones
+   def == (fraccion)
+      @num == fraccion.num && @denom == fraccion.denom
+   end
 end
+
+###################################
+
+# a = Fraccion.new(2,3)
+# b = Fraccion.new(3,8)
+# 
+# puts a.to_s
+# puts b.to_s
+# 
+# puts a.suma(b).to_s
+# puts a.resta(b).to_s
+# puts a.producto(b).to_s
+# puts a.division(b).to_s
+
+###################################
+
+
--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,23 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+   def test_suma
+      assert_equal(Fraccion.new(25,24),Fraccion.new(2,3).suma(Fraccion.new(3,8)))
+      assert_equal(Fraccion.new(31,7),Fraccion.new(8,2).suma(Fraccion.new(3,7)))
+   end
 
+   def test_resta 
+      assert_equal(Fraccion.new(7,24),Fraccion.new(2,3).resta(Fraccion.new(3,8)))
+      assert_equal(Fraccion.new(25,7),Fraccion.new(8,2).resta(Fraccion.new(3,7)))
+   end
+
+   def test_producto
+      assert_equal(Fraccion.new(1,4),Fraccion.new(2,3).producto(Fraccion.new(3,8)))
+      assert_equal(Fraccion.new(12,7),Fraccion.new(8,2).producto(Fraccion.new(3,7)))
+   end
+
+   def test_division
+      assert_equal(Fraccion.new(16,9),Fraccion.new(2,3).division(Fraccion.new(3,8)))
+      assert_equal(Fraccion.new(28,3),Fraccion.new(8,2).division(Fraccion.new(3,7)))
+   end
 end
